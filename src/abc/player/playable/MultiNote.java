3892cc85ac29682d3e6eeed6d7aa8418ca06eb0d
James Gilles <jhgilles@mit.edu>
2015-11-29 21:47:49 -0500

    Add Playable implementers

---
+++ src/abc/player/playable/MultiNote.java
@@ -0,0 +1,21 @@
+package abc.player.playable;
+
+import abc.player.Fraction;
+import abc.player.Playable;
+import abc.sound.SequencePlayer;
+
+public class MultiNote implements Playable {
+
+    @Override
+    public Fraction getLength() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void addToPlayer(SequencePlayer player, int ticksPerBeat, int startTick) throws IllegalArgumentException {
+        // TODO Auto-generated method stub
+
+    }
+
+}
