f39993392f41d314c9a1af75043845949b9a7254
James Gilles <jhgilles@mit.edu>
2015-11-29 20:30:25 -0500

    Implemented first warmup piece

--- test/abc/sound/SequencePlayerTest.java
+++ test/abc/sound/SequencePlayerTest.java
@@ -9,18 +9,15 @@ import javax.sound.midi.MidiUnavailableException;
 
 import org.junit.Test;
 
+/**
+ * @category no_didit
+ */
 public class SequencePlayerTest {
-
-    // TODO: warmup #2
     
     @Test
-    public void test() {
-        
-        fail("Not yet implemented");
-    }
-    
-    @Test
-    public void testPiece2() throws MidiUnavailableException, InvalidMidiDataException, IOException {
+    public void testPieceTwo() throws MidiUnavailableException,
+            InvalidMidiDataException,
+            InterruptedException {
         SequencePlayer player = new SequencePlayer(200, 12);
 
         //MEASURE 1
@@ -90,6 +87,83 @@ public class SequencePlayerTest {
         System.out.println(player);
         
         player.play();
-        System.in.read();
+        
+        Thread.sleep(7000);
+    }
+
+    @Test
+    public void testPlayPieceOne() throws MidiUnavailableException,
+            InvalidMidiDataException,
+            InterruptedException {
+        int C = new Pitch('C').toMidiNote();
+        int D = new Pitch('D').toMidiNote();
+        int E = new Pitch('E').toMidiNote();
+        int F = new Pitch('F').toMidiNote();
+        int G = new Pitch('G').toMidiNote();
+        int c = new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote();
+        
+        final SequencePlayer player = new SequencePlayer(140, 12);
+
+        // C
+        player.addNote(C, 0, 12);
+        // C
+        player.addNote(C, 12, 12);
+        // C3/4
+        player.addNote(C, 24, 9);
+        // D/4
+        player.addNote(D, 33, 3);
+        // E
+        player.addNote(E, 36, 12);
+
+        // E3/4
+        player.addNote(E, 48, 9);
+        // D/4
+        player.addNote(D, 57, 3);
+        // E3/4
+        player.addNote(E, 60, 9);
+        // F/4
+        player.addNote(F, 69, 3);
+        // G2
+        player.addNote(G, 72, 24);
+
+        // c1/2
+        player.addNote(c, 96, 4);
+        // c1/2
+        player.addNote(c, 100, 4);
+        // c1/2
+        player.addNote(c, 104, 4);
+        // G1/2
+        player.addNote(G, 108, 4);
+        // G1/2
+        player.addNote(G, 112, 4);
+        // G1/2
+        player.addNote(G, 116, 4);
+        // E1/2
+        player.addNote(E, 120, 4);
+        // E1/2
+        player.addNote(E, 124, 4);
+        // E1/2
+        player.addNote(E, 128, 4);
+        // C1/2
+        player.addNote(C, 132, 4);
+        // C1/2
+        player.addNote(C, 136, 4);
+        // C1/2
+        player.addNote(C, 140, 4);
+        
+        // G3/4
+        player.addNote(G, 144, 9);
+        // F/4
+        player.addNote(F, 153, 3);
+        // E3/4
+        player.addNote(E, 156, 9);
+        // D/4
+        player.addNote(D, 165, 3);
+        // C2
+        player.addNote(C, 168, 24);
+        
+        player.play();
+        
+        Thread.sleep(7000);
     }
 }
