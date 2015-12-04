199fcc80ae18997bbfe11eba06abce23fd696a93
Ryan Berg <ryanberg@mit.edu>
2015-12-04 13:36:37 -0500

    implemented remainder of Note

--- src/abc/player/playable/Note.java
+++ src/abc/player/playable/Note.java
@@ -2,11 +2,12 @@ package abc.player.playable;
 
 import abc.player.Fraction;
 import abc.player.Playable;
+import abc.sound.Pitch;
 import abc.sound.SequencePlayer;
 
 public class Note implements Playable {
 
-    private final int pitch; //The pitch of the note, in a format that can be fed directly to player.addNote
+    private final Pitch pitch; //The pitch of the note, in a format that can be fed directly to player.addNote
     private final Fraction length; //The length of the note, as a fraction of a beat
     
     /**
@@ -14,8 +15,8 @@ public class Note implements Playable {
      * @param pitch The pitch of the note, in a format that can be fed directly to player.addNote 
      * @param length The length of the note, as a fraction of a beat
      */
-    public Note(int pitch, Fraction length){
-        this.pitch = pitch;
+    public Note(char c, int semitonesUp, Fraction length){
+        this.pitch = new Pitch(c).transpose(semitonesUp);
         this.length = length;
     }
     
@@ -26,7 +27,11 @@ public class Note implements Playable {
 
     @Override
     public int addToPlayer(SequencePlayer player, int ticksPerBeat, int startTick) throws IllegalArgumentException {
+        int noteDuration = ticksPerBeat * length.numerator() / length.denominator();
+        player.addNote(pitch.toMidiNote(), startTick, noteDuration);
         
+        
+        return noteDuration;
     }
 
 }
