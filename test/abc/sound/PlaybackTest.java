344d8627b6f70efb4e7f9789bfae1fff70800ffd
Loren Sherman <lsherman@mit.edu>
2015-12-04 14:01:12 -0500

    Added simple playback test for Playable.addToPlayer

---
+++ test/abc/sound/PlaybackTest.java
@@ -0,0 +1,50 @@
+package abc.sound;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+
+import org.junit.Test;
+
+import abc.player.Fraction;
+import abc.player.Playable;
+import abc.player.playable.Concat;
+import abc.player.playable.MultiNote;
+import abc.player.playable.Note;
+import abc.player.playable.Rest;
+
+/**
+ * Test to make sure the Playable's addToPlayer method works properly
+ * @category no_didit 
+ */
+public class PlaybackTest {
+
+    @Test
+    public void testPlayback() throws MidiUnavailableException,
+            InvalidMidiDataException,
+            InterruptedException {
+        
+        //Construct a series of notes
+        List<Note> chord = new ArrayList<>();
+        chord.add(new Note(new Pitch('C').transpose(Pitch.OCTAVE),new Fraction(1,8)));
+        chord.add(new Note(new Pitch('G'),new Fraction(1,8)));
+        chord.add(new Note(new Pitch('C'),new Fraction(1,2)));
+        
+        List<Playable> concatList = new ArrayList<>();
+        concatList.add(new MultiNote(chord));
+        concatList.add(new Rest(new Fraction(1,8)));
+        concatList.add(new Note(new Pitch('C').transpose(Pitch.OCTAVE),new Fraction(1,4)));
+        
+        Concat masterConcat = new Concat(concatList);
+        
+        SequencePlayer player = new SequencePlayer(200, 12);
+        masterConcat.addToPlayer(player, 8, 0);
+        player.play();
+        
+        Thread.sleep(4000);
+        
+    }
+    
+}
